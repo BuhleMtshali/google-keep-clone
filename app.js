f2871b20cec96486edb818409d656d289256a576
Buhle <mtshalizanobuhle7@gmail.com>
2025-01-30 19:17:10 +0200

    updated the javascript file

--- app.js
+++ app.js
@@ -7,3 +7,18 @@ menuBars.addEventListener('click', () => {
         menuBars.className = "fa-solid fa-bars"
     }
 })
+
+// //adding functionality for the focus on the input element form
+let searchForm = document.getElementById('search-form');
+let searchIcon = document.getElementById('search-icon');
+
+//event listner for listening
+searchForm.addEventListener('click', () => {
+    searchForm.classList.toggle('focus-form')
+    if(searchForm.classList.contains('focus-form')){
+        searchIcon.style.color = '#5F6368'
+    } else{
+        searchIcon.style.color = 'white'
+    }
+})
+
